b45dfed0cb66b6add001ec9bafaa509e4424b93d
Egor Kuchinskii <bifrost.step@gmail.com>
2024-11-05 21:53:49 +0300

    front: цвет иконки спецпредложения

--- frontend/src/shared/ui/icons/ui/specialOffers.tsx
+++ frontend/src/shared/ui/icons/ui/specialOffers.tsx
@@ -7,7 +7,7 @@ export const IconSpecialOffers = ({ className }: { className?: string }) => (
     width="14"
     height="22"
     viewBox="0 0 12 18"
-    fill="#0153F4"
+    fill="#8B29C0"
   >
     <path
       d="M1 9.25692L6.2381 1L11 9.25692M1 9.25692L6.2381 17L11 9.25692M1 9.25692L6.2381 10.5238L11 9.25692"
